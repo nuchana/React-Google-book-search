19cf79b86032207f8a705c01f4e4c885289cc58f
Nuchana <nuchana.johnson@gmail.com>
2021-01-08 22:39:16 -0500

    update page search.js

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -9,7 +9,7 @@ import { Input, SearchButton } from "../components/Input";
 class Search extends Component {
   // instatiate state for list of books retrieved from googlebooks api and bookSearch value
   state = {
-    
+
     books: [],
     bookSearch: "",
     message: "Search For A Book To Begin!"
@@ -24,20 +24,74 @@ class Search extends Component {
     });
   };
 
- 
-
   handleFormSubmit = event => {
     event.preventDefault();
     API.searchBooks(this.state.bookSearch)
-      .then (res => {
+      .then(res => {
         this.setState({ books: res.data.items }, function () {
           console.log(this.state.books);
-          
+
         })
       })
-      .catch (err => console.log (err))
+      .catch(err => console.log(err))
   };
 
-}
+  render() {
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Col size="md-12">
+              <form>
+                <Container>
+                  <Row>
+                    <Col size="xs-12 sm-12">
+                      <Input
+                        name="bookSearch"
+                        value={this.state.bookSearch}
+                        onChange={this.handleInputChange}
+                        placeholder="Search for a Book"
+                      />
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col size="xs-12 sm-12">
+                      <SearchButton
+                        onClick={this.handleFormSubmit}
+                        type="success"
+                        className="input-lg"
+                      >Search
+                      </SearchButton>
+                    </Col>
+                  </Row>
+                </Container>
+              </form>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="xs-12">
+              <BookList>
+                {this.state.books.map(book => {
+                  return (
+                    <BookListItem
+                      key={book.id}
+                      title={book.volumeInfo.title}
+                      authors={book.volumeInfo.authors}
+                      link={book.volumeInfo.infoLink}
+                      description={book.volumeInfo.description}
+                      // if no imageLinks then use placeholder image
+                      image={book.volumeInfo.imageLinks === undefined ? "http://siddallheatingandcooling.net/_imgstore/5/1360415/thumbnail/FSeY96wEdX_eY4XkBN2jfYnuY9A.png" : `${book.volumeInfo.imageLinks.thumbnail}`}
+                    />);
+                })}
+              </BookList>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  };
+
+};
 
 export default Search;
+
