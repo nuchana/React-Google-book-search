71fb7f7c13c47a22f473138f9465d418c4a2e9ac
Nuchana <nuchana.johnson@gmail.com>
2021-01-08 23:06:38 -0500

    create models folder

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,41 +1,30 @@
 const db = require("../models");
-const axios = require("axios");
 
-// Defining methods for the bookController
 module.exports = {
-  
-    // function to use API from backend
-    searchApi: function (req, res) {
-    axios.get("https://www.googleapis.com/books/v1/volumes?q=flowers")
-      .then((response) => res.json(response.data))
-      .catch(err => res.status(422).json(err))
-  
-  },
-  
-  findAll: function(req, res) {
-    db.Book.find(req.query)
-      .then(dbBook => res.json(dbBook))
+  findAll: function (req, res) {
+    db.GoogleBooks
+      .find(req.query)
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  findById: function(req, res) {
-    db.Book.findById(req.params.id)
-      .then(dbBook => res.json(dbBook))
+  findOne: function (req, res) {
+    db.GoogleBooks
+      .findOne({ id: req.params.id })
+      .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  save: function(req, res) {
-    db.Book.create(req.body)
-      .then(dbBook => res.json(dbBook))
+  save: function (req, res) {
+    db.GoogleBooks
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
-  update: function(req, res) {
-    db.Book.findOneAndUpdate({ id: req.params.id }, req.body)
-      .then(dbBook => res.json(dbBook))
-      .catch(err => res.status(422).json(err));
-  },
-  remove: function(req, res) {
-    db.Book.findById(req.params.id)
-      .then(dbBook => dbBook.remove())
-      .then(dbBook => res.json(dbBook))
+  remove: function (req, res) {
+    db.GoogleBooks
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   }
 };
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,4 +1,5 @@
 /* eslint-disable import/no-anonymous-default-export */
+
 // Required module
 import axios from "axios";
 
